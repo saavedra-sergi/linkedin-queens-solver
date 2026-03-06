08a4e905125e038ccfb59f2c1371a14dda310920
Sergi <sergifangoku@gmail.com>
2026-03-06 15:36:00 +0100

    Add examples in json files

--- src/main.py
+++ src/main.py
@@ -1,29 +1,28 @@
+import json
+import sys
 from board import Board
 from solver import solve
 
-
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <puzzle_file.json>")
+        return
 
-    regions = [
-            [1,1,1,3,3,3,3,3,3],      #this board is an example, board will be given as an input
-            [2,2,1,3,3,3,3,3,3],
-            [2,2,1,3,3,3,4,4,4],
-            [3,3,3,3,3,3,5,5,4],
-            [3,6,6,7,3,3,5,5,5],
-            [3,7,6,7,3,3,3,3,3],
-            [3,7,7,7,8,8,9,3,3],
-            [3,3,3,3,8,8,9,3,3],
-            [3,3,3,3,9,9,9,3,3]
-    ]
+    puzzle_file = sys.argv[1]
 
-    board = Board(9, regions)
+    with open(puzzle_file) as f:
+        data = json.load(f)
 
-    if solve(board, 0):
+    size = data["size"]
+    regions = data["regions"]
+
+    board = Board(size, regions)
+
+    if solve(board):
         print("Solution found:\n")
         board.print_board()
     else:
         print("No solution")
-
 
 if __name__ == "__main__":
     main()
