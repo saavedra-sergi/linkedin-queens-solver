621fa7b52b5c4a81d4424e8e561f997aa6ffe30b
Sergi <sergifangoku@gmail.com>
2026-03-06 12:40:17 +0100

    Test with a 9x9

--- src/main.py
+++ src/main.py
@@ -5,17 +5,18 @@ from solver import solve
 def main():
 
     regions = [
-            [1,1,1,1,1,1,1,1],      #this board is an example, board will be given as an input
-            [1,2,3,3,3,3,3,1],
-            [1,2,3,3,3,3,4,1],
-            [5,5,5,3,3,3,4,1],
-            [6,5,3,3,3,4,4,4],
-            [6,5,3,3,3,7,8,7],
-            [6,6,6,6,7,7,8,7],
-            [6,6,6,6,7,7,7,7]
+            [1,1,1,3,3,3,3,3,3],      #this board is an example, board will be given as an input
+            [2,2,1,3,3,3,3,3,3],
+            [2,2,1,3,3,3,4,4,4],
+            [3,3,3,3,3,3,5,5,4],
+            [3,6,6,7,3,3,5,5,5],
+            [3,7,6,7,3,3,3,3,3],
+            [3,7,7,7,8,8,9,3,3],
+            [3,3,3,3,8,8,9,3,3],
+            [3,3,3,3,9,9,9,3,3]
     ]
 
-    board = Board(8, regions)
+    board = Board(9, regions)
 
     if solve(board, 0):
         print("Solution found:\n")
